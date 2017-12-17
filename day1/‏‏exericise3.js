3b92398751a714877c69512090239488044d4bf6
noavish <noavish1@gmail.com>
2017-12-18 00:58:24 +0200

    fix file names

--- day1/‏‏exericise3.js
+++
@@ -1,6 +0,0 @@
-var myColor = ["Red", "Green", "White", "Black"];
-var stringToConsole = myColor[0];
-for (var i=1; i<myColor.length; i++) {
-    stringToConsole += " + " + myColor[i];
-}
-console.log (stringToConsole);
